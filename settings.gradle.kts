8a3b1262482b45bee1938ff8560f5f2f602e30b4
Xiujun Ma <xma@compusoftdevelopment.com>
2023-12-08 14:08:18 -0700

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,7 @@
+rootProject.name = "todo"
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+    }
+}
---
+++ src/wasmJsMain/kotlin/todo.kt
@@ -0,0 +1,13 @@
+import kotlinx.browser.window
+import org.w3c.dom.*
+
+class TodoApp(window: Window, document: Document, storage: Storage, inputBox: HTMLInputElement, button: HTMLButtonElement, list: HTMLUListElement) {
+    fun addTask() {
+
+    }
+}
+
+fun main() {
+    println("Hello, Kotlin/JS!")
+    window.alert("Hello, Kotlin/JS!")
+}
